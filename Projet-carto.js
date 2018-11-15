8df180aa3d31ed0476300e46ae77c7fce30ee9fa
Mmetayer1 <45002579+Mmetayer1@users.noreply.github.com>
2018-11-15 17:21:44 +0100

    Add files via upload

---
+++ Projet-carto.js
@@ -0,0 +1,7 @@
+console.log("Bonjour")
+mapboxgl.accessToken ="pk.eyJ1IjoibW1ldGF5ZXIiLCJhIjoiY2pvOGp4YmxjMGFqdjNwbGVyNHptbm9zNSJ9.SODxEtfIaL9nSP0JnndLww";
+console.log("au revoir")
+var map = new mapboxgl.Map({
+  container: "map", // container id
+  style: "mapbox://styles/mmetayer/cjo8m6rkz3i9c2rme130est90" // replace this with your style URL
+});
